3eb024760292955afa780a8642e3e6729e4b0fd7
“Awanee <Awanee.pandey@gmail.com>
2021-01-15 23:46:18 -0600

    Third commit

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const searchInput = document.getElementById('search'),
             } else{
                 meals.innerHTML = data.meals.map(meal =>
                     `<div class ="meal">
-                        <img src='${meal.strMealThumb}' href='${meal.strMeal}'>
+                        <img src='${meal.strMealThumb}' href='${meal.strMeal}' />
                         <div class = "meal-info" data-mealID= "${meal.idMeal}">
                             <h3>${meal.strMeal}</h3>
                         </div>
@@ -43,9 +43,48 @@ const searchInput = document.getElementById('search'),
     }
  }
 
+/* Fetch meal by ID */
+function getMealById(mealID){
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then(res=>res.json())
+        .then(data =>{
+            const meal =data.meals[0];
+            addMealToDOM(meal);
+        });
+}
+
+/* Add meal to DOM */
+function addMealToDOM(meal){
+    const ingredients = [];
+    for(let i =0 ;i<20;i++){
+        if(meal[`strIngredient${i}`]){
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+        }else{
+            break;
+        }
+    }
+
+    singleMeal.innerHTML=`
+        <div class = "single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src='${meal.strMealThumb}' href='${meal.strMeal}' />
+            <div class='single-meal-info'>
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients</h2>
+                <ul>
+                    ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+                </ul>
+            </div>
+        
+    `
+}
+
  /* Event Listener */
  searchbtn.addEventListener('click',searchMeal);
-
 
  /* Individual data showing part  */
  meals.addEventListener('click',e=>{
